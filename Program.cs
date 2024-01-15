69124f660c22cd6ff49369bad1f1e053dc9dda8d
Joao Quiziri <quizirironchi@hotmail.com>
2024-01-15 19:07:00 -0300

    finish commit

--- Program.cs
+++ Program.cs
@@ -18,6 +18,7 @@ class Program
         Console.WriteLine("2 - Subtração");
         Console.WriteLine("3 - Divisão");
         Console.WriteLine("4 - Multiplicação");
+        Console.WriteLine("5 - Sair");
 
         Console.WriteLine("----------");
         Console.WriteLine("Selecione uma opção: ");
@@ -29,6 +30,7 @@ class Program
             case 2: Subtracao(); break;
             case 3: Divisao(); break;
             case 4: Multiplicacao(); break;
+            case 5: Environment.Exit(0); break;
             default: Menu(); break;
         }
     }
